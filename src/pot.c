95c137233da9400ff1a234eebd96dae87b2fc990
Lev0071 <coder0071@outlook.com>
2025-06-02 02:25:12 +1000

    edit print_pot(..) for readablilty during testing

--- src/pot.c
+++ src/pot.c
@@ -20,7 +20,11 @@ void add_eligible_player(Pot *pot,Player *player){
 void print_pot(const Pot *pot){
     printf("Pot: %d credits | Eligible: ", pot->amount);
     for(int i = 0; i < pot->num_players;i++){
-        printf("%s ",pot->eligible_players[i]->name);
+        if(i != pot->num_players -1){
+            printf("%s, ",pot->eligible_players[i]->name);
+        }else{
+            printf("%s.",pot->eligible_players[i]->name);
+        }
     }
     printf("\n");
 }
